533def536e2be084e1eb9083be0a9d585761ac46
Leonardo Souza <leonardo-f98@hotmail.com>
2024-08-01 18:17:58 -0300

    update port number

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Backend Orange Portifolio')
@@ -13,7 +15,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(port);
 }
 
 bootstrap();
